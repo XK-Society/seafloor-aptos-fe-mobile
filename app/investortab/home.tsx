32f85a2804fa9ddfab51c69a29d6a8e043349020
Wafiqah Saat <wafiqahsaat@gmail.com>
2024-09-29 05:34:32 +0800

    add return Homepage button in Show Tokenize

--- app/investortab/home.tsx
+++ app/investortab/home.tsx
@@ -8,7 +8,6 @@ import { Colors } from '../../constants/Colors'
 export default function InvestorHome() {
   return (
     <View style={styles.container}>
-      <InvestorHeader />
       <InvHome />
     </View>
   )
